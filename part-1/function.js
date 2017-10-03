ed3300eab4f47ea32a39e0a9107bf14c4d24e71f
bj bautista <emailme@djbrigidope.com>
2017-10-02 22:37:03 -0700

    am i using the date object wrong?

--- part-1/function.js
+++ part-1/function.js
@@ -1,8 +1,26 @@
-function month(date) {
+const month = date => {
+  let d = new Date(date);
+  let dateMonth = d.getMonth();
+  const monthArr = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sept',
+    'Oct',
+    'Nov',
+    'Dec'
+  ];
+  return monthArr[dateMonth];
 
 }
-
-
+//
+// console.log(month(2017, 11, 25));
+// console.log(month(1980, 08, 19));
 
 
 
--- part-1/test/test.js
+++ part-1/test/test.js
@@ -4,5 +4,9 @@ const month = require('../function.js');
 describe('month',()=>{
   it('is a function',()=>{
     expect(month).to.be.a('function')
-  })
+  });
+  it('returns the month when provided a date',()=>{
+
+  });
+  it('returns')
 })
